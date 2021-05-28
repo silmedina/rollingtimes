b9dc373a8421fc7c9b061886a79d8f2f49b01511
sil Medina <medina.silviapatricia@gmail.com>
2021-05-28 02:40:22 -0300

    se agrega archivo env con url de categorias y se obtienen las categorias en el componente app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,29 @@ import Navegacion from "./components/common/Navegacion";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/Inicio";
 import ListarCategorias from "./components/ListarCategorias";
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [categorias, setCategorias] = useState([]);
+  
+  useEffect(()=>{
+    consultarCategorias();
+  },[]);
+
+  const  consultarCategorias = async ()=>{
+    try {
+      const url = process.env.REACT_APP_URL_LISTAR_CATEGORIAS;
+      const respuesta = await fetch( url);
+      const informacion = await respuesta.json();
+      if(respuesta.status === 200){
+        setCategorias(informacion);
+        console.log(informacion)
+      }
+    } catch (error) {
+      console.log(error);      
+    }
+  }
+
   return (
     <Router>
       {/* <Navegacion /> */}
@@ -15,7 +36,7 @@ function App() {
           <Inicio />
         </Route>
         <Route exact path="/categorias">
-          <ListarCategorias />
+          <ListarCategorias categorias={categorias} />
         </Route>
       </Switch>
       <Footer />
