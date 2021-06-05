91cc2b9274fff1d48ca82d298458d16ab7c35730
tulio <tuliomoya17@gmail.com>
2021-06-05 13:22:42 -0300

    se cambio la funcionalidad del navbar

--- src/components/common/Categoria.js
+++ src/components/common/Categoria.js
@@ -7,30 +7,12 @@ import { Navbar, Nav, Form, Button, Container } from 'react-bootstrap';
 const Categoria = () => {
     return (
         <Fragment>
-            <Nav variant="tabs" defaultActiveKey="/home" className='bg5 px-5' id='nav'>
+            <Nav variant="tabs" defaultActiveKey="/home" className='bg5 pt-2 d-flex justify-content-center' id='nav'>
                 <Nav.Item>
                     <Nav.Link className='color1' href="/home">Home</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link className='color1' eventKey="link-1">Ultimo Momento</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-2">Política</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-3">Economía</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-4">Sociedad</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-5">Mundo</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-6">Deportes</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link className='color1' eventKey="link-7">Espectáculos</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link className='color1' eventKey="link-2">Política</Nav.Link>
--- src/components/common/Navegacion.js
+++ src/components/common/Navegacion.js
@@ -15,9 +15,13 @@ const Navegacion = () => {
     const [compactNav, setcompactNav] = useState(false);
 
     const cambiarNav = () => {
-        if (window.scrollY >= 20) {
+        if (window.scrollY >= 30) {
+            const nuevaClaseExpa = document.getElementById('navExpand');
+            nuevaClaseExpa.style.display = "none";
             setcompactNav(true);
-        } else {
+        } else if (window.scrollY < 30) {
+            const nuevaClaseExpa = document.getElementById('navExpand');
+            nuevaClaseExpa.style.display = "block";
             setcompactNav(false);
         }
     }
@@ -30,34 +34,33 @@ const Navegacion = () => {
     const Mobile = ({ children }) => {
         const isMobile = useMediaQuery({ minWidth: 350, maxWidth: 991 })
         return isMobile ? children : null
-    } 
-
-
+    }
 
 
     return (
         <Fragment>
             <Desktop>
                 <div className="d-flex flex-column">
-                    <Navbar collapseOnSelect expand="lg" className="bg5" id='navBlock' >
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        {(compactNav === true) ? (<Menudespleg/>) : (null)}
-
-                        {(compactNav === true) ? (<img className='mr-3' src={logoSm} alt="logo" />) : (null)
-                        }
-                        <Form inline className=''>
-                            <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button className='' variant="outline-dark"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Button>
-                        </Form>
-                        <Nav className='ml-auto'>
-                            <Button className='mx-2 my-1' variant='outline-dark'>Ingresar <FontAwesomeIcon icon={faUser}></FontAwesomeIcon></Button>
-                            <Button className='mx-2 my-1' variant='outline-dark'>Suscribite</Button>
-                        </Nav>
-                    </Navbar>
-                    <div>
-                        {(compactNav === false) ? (<Logo/>) : (null)}
-                        {(compactNav === false) ? (<Cotizacion />) : (null)}
-                        {(compactNav === false) ? (<Categoria />) : (null)}
+                    <div className="bg3" id="navDesk">
+                        <Navbar collapseOnSelect expand="lg" className="bg5" id='navBlock' >
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            {(compactNav === true) ? (<Menudespleg />) : (null)}
+                            {(compactNav === true) ? (<img className='mr-3' src={logoSm} alt="logo" />) : (null)
+                            }
+                            <Form inline className='m-3'>
+                                <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+                                <Button className='' variant="outline-dark"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Button>
+                            </Form>
+                            <Nav className='ml-auto'>
+                                <Button className='mx-2 my-1' variant='outline-dark'>Ingresar <FontAwesomeIcon icon={faUser}></FontAwesomeIcon></Button>
+                                <Button className='mx-2 my-1' variant='outline-dark'>Suscribite</Button>
+                            </Nav>
+                        </Navbar>
+                        <div className="" id="navExpand">
+                            <Logo />
+                            <Cotizacion />
+                            <Categoria />
+                        </div>
                     </div>
                 </div>
             </Desktop>
