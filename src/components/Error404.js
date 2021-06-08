51dfd058d1f1a79c3fe83befa1ea078a185f2588
sil Medina <medina.silviapatricia@gmail.com>
2021-06-08 00:33:22 -0300

    se agrega componente spinner

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -8,20 +8,22 @@ import error404 from "../assets/error404.gif";
 const Error404 = () => {
   return (
     <Container className="text-center pt-lg-5 pb-lg-5">
-         <Image src={error404} roundedCircle width={300} alt="Error 404"/>
+      <Image src={error404} roundedCircle width={300} alt="Error 404" />
       <h1 className="text-center mt-lg-5 mb-lg-5">Pagina no encontrada</h1>
       <h5 className="text-center">Disculpa, esta pagina no esta disponible</h5>
       <h6 className="text-center mb-lg-4">
         Error 404: la direccion ingresada no es correcta
       </h6>
-      <Link className="text-center" to={`/contacto`}>
-        <FontAwesomeIcon icon={faEnvelopeOpenText}></FontAwesomeIcon> Reportar
-        enlace
-      </Link>{" "}
-      |{" "}
-      <Link className="text-center" to={`/`}>
-        <FontAwesomeIcon icon={faHome}></FontAwesomeIcon> Ir al Inicio
-      </Link>
+      <div id="notfoundlinks">
+        <Link className="text-center" to={`/contacto`} id="contacto404">
+          <FontAwesomeIcon icon={faEnvelopeOpenText}></FontAwesomeIcon> Reportar
+          enlace
+        </Link>{" "}
+        |{" "}
+        <Link className="text-center" to={`/`}>
+          <FontAwesomeIcon icon={faHome}></FontAwesomeIcon> Ir al Inicio
+        </Link>
+      </div>
     </Container>
   );
 };
