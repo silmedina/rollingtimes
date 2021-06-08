94927966131a75ad24c71e82a8c4e7521cfbd5ab
jardodrani <jardodrani@gmail.com>
2021-06-08 17:10:03 -0300

    -

--- src/components/Inicio/CarouselNoticias.js
+++ src/components/Inicio/CarouselNoticias.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
-import './carouselNoticias.css';
+import "./carouselNoticias.css";
 
 const CarouselNoticias = () => {
   const [index, setIndex] = useState(0);
@@ -34,32 +34,26 @@ const CarouselNoticias = () => {
   return (
     <div>
       <Carousel activeIndex={index} onSelect={handleSelect}>
-
         {arrayNoticias.map((noticia, index) => {
           return (
-
-            <Carousel.Item key={index} id='noticiaContainer'>
-                <div className='black-gradient-bg' />
+            <Carousel.Item key={index} id="noticiaContainer">
+              <div className="black-gradient-bg" />
               <img
                 className="d-block w-100 imagen-noticia"
                 src={noticia.imagen}
-                alt={'slide' + index}
+                alt={"slide" + index}
               />
-              <Carousel.Caption id='noticiaCaption'>
-                <div id='noticiaInfo'>
-                    <p>{'categoría'}</p>
-                    <p>{'fecha'}</p>
+              <Carousel.Caption id="noticiaCaption" style={{cursor: 'pointer'}}>
+                <div id="noticiaInfo">
+                  <p>{"categoría"}</p>
+                  <p>{"fecha"}</p>
                 </div>
                 <h3>{noticia.titulo}</h3>
-                <p>
-                  {noticia.descripcion}
-                </p>
+                <p>{noticia.descripcion}</p>
               </Carousel.Caption>
             </Carousel.Item>
-
           );
         })}
-
       </Carousel>
     </div>
   );
