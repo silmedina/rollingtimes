9d9725d9d4162bcfff83cb28c82abeae191a9380
sil Medina <medina.silviapatricia@gmail.com>
2021-07-01 02:19:35 -0300

    home cantidad de noticas por fila

--- src/components/Inicio/BoardNoticias.js
+++ src/components/Inicio/BoardNoticias.js
@@ -1,35 +1,43 @@
 import React from "react";
 import { Card, CardDeck, Container } from "react-bootstrap";
-import Moment from 'moment';
+import Moment from "moment";
 
 const BoardNoticias = (props) => {
   const formatearFecha = (fecha) => {
-    Moment.locale('es');
-    let fechaFormateada = Moment(fecha).format('LLLL');
+    Moment.locale("es");
+    let fechaFormateada = Moment(fecha).format("LLLL");
     return fechaFormateada;
-  }
+  };
 
   return (
     <Container className="mb-4 mt-4">
-      <CardDeck>
-        {props.noticias.map((noticia) => (
-          <Card>
-            <Card.Img variant="top" src={noticia.imagen} />
-            <Card.Body>
-              <Card.Title>{noticia.titulo}</Card.Title>
-              <Card.Text>
-                {noticia.subtitulo}
-                <div id="notfoundlinks" className="mt-3">
-                  <Card.Link href={`/noticia/${noticia._id}`}>Ir a la noticia</Card.Link>
-                </div>
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">{formatearFecha(noticia.fecha)}</small>
-            </Card.Footer>
-          </Card>
-        ))}
-      </CardDeck>
+      <div className="row">
+        <CardDeck>
+          {props.noticias.map((noticia) => (
+            <div className="col-md-4 mb-3">
+              <Card>
+                <Card.Img variant="top" src={noticia.imagen} />
+                <Card.Body>
+                  <Card.Title>{noticia.titulo}</Card.Title>
+                  <Card.Text>
+                    {noticia.subtitulo}
+                    <div id="notfoundlinks" className="mt-3">
+                      <Card.Link href={`/noticia/${noticia._id}`}>
+                        Ir a la noticia
+                      </Card.Link>
+                    </div>
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">
+                    {formatearFecha(noticia.fecha)}
+                  </small>
+                </Card.Footer>
+              </Card>
+            </div>
+          ))}
+        </CardDeck>
+      </div>
     </Container>
   );
 };
