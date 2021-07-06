d5f2793df88e66ea34d361b83b8f51433c267866
David Sarhid <daviidsarhid@gmail.com>
2021-07-06 01:28:53 -0300

    diseño about

--- src/components/AcercaDe/AboutUs.js
+++ src/components/AcercaDe/AboutUs.js
@@ -5,56 +5,56 @@ import SIL from "../../assets/SIL.jpg";
 import BENJA from "../../assets/BENJA.jpeg";
 import DAVID from "../../assets/DAVID.jpg";
 import TULIO from "../../assets/TULIO.jpeg";
+import ABOUT from "../../assets/about-us2.png"
 
 const AboutUs = () => {
   return (
     <div className="text-center bg5">
-      <div className="">
-        <h1 className="py-5">Acerca de nosotros</h1>
-        <div className="d-flex justify-content-center align-items-center">
-          <div className="row justify-content-center">
-            <div className="m-3">
+        <div>
+        <h1 className="py-4"><img src={ABOUT} alt="about-us" className="w-100"></img></h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="row p-0 m-0">
+            
+            <div className="my-3 px-0 mx-0 col-sm-12 col-md-6 col-lg-3 px-5">
               <AboutUsCard
                 image={SIL}
                 title="Sil Medina"
                 text1="Developer"
                 text2='Customer Cure Representative and QA trainee at Rollingcode "Se un punto de referencia de calidad" '
-                width={"14rem"}
-                className=""
+                width={"220px"}
               />
             </div>
-
-            <div className="m-3">
+            <div className="my-3 px-0 mx-0 col-sm-12 col-md-6 col-lg-3 px-5">
               <AboutUsCard
                 image={TULIO}
                 title="Tulio Moya"
                 text1="Developer"
-                text2="Frase"
-                width={"13rem"}
+                text2="Junior developer FullStack and economy student"
+                width={"220px"}
               />
             </div>
-            <div className="m-3">
+            <div className="my-3 px-0 mx-0 col-sm-12 col-md-6 col-lg-3 px-5">
               <AboutUsCard
                 image={DAVID}
                 title="David Sarhid"
                 text1="Developer"
-                text2="Frase"
-                width={"14rem"}
+                text2="Junior developer FullStack and systems engineering student"
+                width={"220px"}
               />
             </div>
-            <div className="m-3">
+            <div className="my-3 px-0 mx-0 col-sm-12 col-md-6 col-lg-3 px-5">
               <AboutUsCard
                 image={BENJA}
                 title="Benjamín Dracnizarek"
                 text1="Developer"
-                text2='Frase '
-                width={"14rem"}
+                text2='Junior developer FullStack and freelancer to music'
+                width={"220px"}
               />
             </div>
+            
           </div>
         </div>
-      </div>
-      <div className=""></div>
     </div>
   );
 };
